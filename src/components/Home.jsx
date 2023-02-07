09ace27bf487a0ff917548bd298a631d3f9dd90d
Sebastian Luna <sebas1997321@gmail.com>
2023-02-07 14:30:33 -0500

    home preview

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,20 @@
-import React, { useEffect } from 'react'
+import { Flex } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
 import Header from "../containers/Header"
 import List from "../containers/List"
-import Map from "../containers/Map"
 import Place from "../containers/Place"
 
+const places = [
+    {name: 'place1'}
+]
+
 const Home = () => {
+    const [coordinates, setCoordinates] = useState({lat: 0, lng: 0})
+    const [type, setType] = useState('restaurants')
+    const [ratings, setRatings] = useState("")
+    const [isLoading, setIsLoading] = useState(true)
+
+    
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
             console.log(latitude, longitude)
@@ -12,8 +22,21 @@ const Home = () => {
         
     }, [])
 
-    return (
-        <div>Home</div>
-    )
+    return <Flex
+        justifyContent={"center"}
+        alignItems={"center"}
+        width={"100vw"}
+        height={"100vh"}
+        maxWidth={"100vw"}
+        maxHeight={"100vh"}
+        position={"relative"}
+    >
+      <Header 
+        type={setType}
+        setCoordinates={setCoordinates}
+        setRatings={setRatings}
+      />
+      <List places= {places} isLoading={isLoading}/>
+    </Flex>
 }
 export default Home
--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,9 +1,134 @@
+import { Flex, Input, InputGroup, InputRightElement, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react'
+import { Rating } from '@material-ui/lab'
+import { Autocomplete } from '@react-google-maps/api'
 import React from 'react'
+import { BiChevronDown, BiSearch, BiStar } from 'react-icons/bi'
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
+const Header = ({ setType, setRatings, setCoordinates }) => {
+    return <Flex
+        position="absolute"
+        top="0"
+        left="0"
+        width="full"
+        px={4}
+        py={2}
+        zIndex={101}
+        justifyContent="space-between"
+    >
+        {/* <Autocomplete> */}
+        <InputGroup
+            width={"35vw"}
+            shadow="lg"
+        >
+            <InputRightElement
+                pointerEvents={"none"}
+                children={<BiSearch color='gray' fontSize={20} />}
+            />
+
+            <Input
+                type="text"
+                placeholder="Search for"
+                variant={'filled'}
+                fontSize={18}
+                bg={'white'}
+                color={'gray.700'}
+                _hover={{ bg: 'whiteAlpha.800' }}
+                _focus={{ bg: 'whiteAlpha.800' }}
+                _placeholder={{ color: 'gray.700' }}
+            >
+            </Input>
+        </InputGroup>
+        {/* </Autocomplete> */}
+        <Flex
+            alignItems={'center'}
+            justifyContent={"center"}
+        >
+            <Flex
+                alignItems={'center'}
+                justifyContent={"center"}
+                px={4}
+                py={2}
+                bg={'white'}
+                rounded={'full'}
+                ml={4}
+                shadow="lg"
+                cursor={"pointer"}
+            >
+                <Menu>
+                    <BiStar fontSize={25} />
+                    <MenuButton mx={2} transitions={'all 0.2s'} borderRadius={'md'}>
+                        Choose Ratings
+                    </MenuButton>
+
+                    <MenuList>
+                        <MenuItem
+                            display={"flex"}
+                            alignItems={"center"}
+                            justifyContent="space-around"
+                            onClick={() => setRatings("")}
+                        >
+                            <Text fontSize={20} fontWeight={500} color={"gray.700"}>
+                                All Rates
+                            </Text>
+                        </MenuItem>
+
+                        <MenuItem
+                            display={"flex"}
+                            alignItems={"center"}
+                            justifyContent="space-around"
+                            onClick={() => setRatings(2)}
+                        >
+                            <Text fontSize={20} fontWeight={500} color={"orange.500"}>
+                                2.0
+                            </Text>
+
+                            <Rating size="small" value={2} readOnly />
+                        </MenuItem>
+
+                        <MenuItem
+                            display={"flex"}
+                            alignItems={"center"}
+                            justifyContent="space-around"
+                            onClick={() => setRatings(3)}
+                        >
+                            <Text fontSize={20} fontWeight={500} color={"orange.500"}>
+                                3.0
+                            </Text>
+
+                            <Rating size="small" value={3} readOnly />
+                        </MenuItem>
+
+                        <MenuItem
+                            display={"flex"}
+                            alignItems={"center"}
+                            justifyContent="space-around"
+                            onClick={() => setRatings(4)}
+                        >
+                            <Text fontSize={20} fontWeight={500} color={"orange.500"}>
+                                4.0
+                            </Text>
+
+                            <Rating size="small" value={4} readOnly />
+                        </MenuItem>
+
+                        <MenuItem
+                            display={"flex"}
+                            alignItems={"center"}
+                            justifyContent="space-around"
+                            onClick={() => setRatings(5)}
+                        >
+                            <Text fontSize={20} fontWeight={500} color={"orange.500"}>
+                                5.0
+                            </Text>
+
+                            <Rating size="small" value={5} readOnly />
+                        </MenuItem>
+                    </MenuList>
+                </Menu>
+                <BiChevronDown fontSize={25} />
+            </Flex>
+        </Flex>
+    </Flex>
 }
 
 export default Header
